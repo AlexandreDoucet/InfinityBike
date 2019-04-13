6ff01e518cb3fb983cc68719e28b87fdfc48d6ba
MxBoud <emb4733@gmail.com>
2019-04-13 17:38:17 -0300

    Implemented the intensity bar manager and game events that sets the target power

--- Infinity Bike/Assets/UI/IntensityBarManager.cs
+++ Infinity Bike/Assets/UI/IntensityBarManager.cs
@@ -6,31 +6,38 @@ using UnityEngine.UI;
 public class IntensityBarManager : MonoBehaviour {
     public ArduinoThread storageValue;
     public Image[] bars;
-    public int maxSpeed = 100; 
-
+    public int maxSpeed = 100;
+    public int target;
+    public int powerInt; 
+    private Blinker blinker; 
 
     private int numBar;
     private int speed;
-public  Color[] colors;
+    public  Color[] colors;
+
 
 	// Use this for initialization
 	void Start () {
         numBar = bars.Length;
+        blinker = GetComponent<Blinker>();
 
-       colors = new Color[numBar];
-        for (int i = 0; i < numBar; i++)
-        {
-            Image bar = bars[i];
-            colors[i] = bar.color;
-        }
+       //colors = new Color[numBar];
+       // for (int i = 0; i < numBar; i++)
+        //{
+            ///colors[i] = b
+        //}
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void SetTargetPower(int targetValue)
+    {
+        target = targetValue;
+    }
+   
+
+    // Update is called once per frame
+    void Update () {
         speed = storageValue.arduinoInfo.arduinoValueStorage.rawSpeed;
 
-
-
+        powerInt = 0; 
         for (int i = 0; i < numBar; i++)
         {
             int j = numBar - i - 1;
@@ -38,14 +45,56 @@ public  Color[] colors;
             float alpha = 0.1f;
 
             if ((float) speed /(float) maxSpeed > (float)(j + 1) /(float) numBar){
-                alpha = 1f; 
+                alpha = 1f;
+                if (j > powerInt)
+                {
+                    powerInt = j;
+                }
+
             }
+
 
 
 
             bar.color = new Color(colors[j].r,colors[j].g, colors[j].b, alpha);
 
         }
+        powerInt++;
+        Debug.Log(powerInt);
+        UpdateTargetPower();
+
+
 		
 	}
+    public void UpdateTargetPower()
+    {
+        for(int j = 0; j<bars.Length; j++)
+        {
+            if (j < target)
+
+            {
+                if (powerInt > j)
+                {
+                    blinker.restState[j] = true;
+                    blinker.isBlinking[j] = false;
+                }
+                else
+                {
+                    blinker.isBlinking[j] = true;
+                    blinker.restState[j] = false;
+                }
+
+
+            }
+            else
+            {
+                blinker.restState[j] = false;
+                blinker.isBlinking[j] = false;
+            }
+
+        }
+
+
+    }
+
 }
